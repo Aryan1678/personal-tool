d7bbc690d0f4fb46b2ce8fefd0d6ad45fefd23ce
Aryan1678 <kumbharearyan18@gmail.com>
2026-09-12 14:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const exploreBtn = document.getElementById("exploreBtn");
+const message = document.getElementById("message");
+
+exploreBtn.addEventListener("click", function () {
+
+    message.textContent = "Welcome! Your journey begins here. ⚔️";
+
+});
